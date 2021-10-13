78ac391529c1cb73348d15a31b5cb3453b82906d
IvanaEspinoza <A01742392@itesm.mx>
2021-10-13 15:26:52 +0000

    proyectp

--- assignments/00Proyecto/src/exercise.py
+++ assignments/00Proyecto/src/exercise.py
@@ -1,8 +1,22 @@
 def leer_datos():
+    paises=[]
+    num_vacunas=[]
+    num_vacunas_poblacion=[]
+    porcen_vacunacion=[]
+    porcen_vacunacion_comple=[]
+    num_vacunas_día=[]
+
     with open('/workspace/Proyecto-eq3/assignments/Global_COVID_Vaccination_Tracker.csv', 'r') as f:  
         for line in f:
-            lista_line_gcv = line.split(",")
-    #Algo mal
+            lista_line= line.split(",")
+
+            paises.append(lista_line[0])
+            num_vacunas.append(lista_line[1])
+            num_vacunas_poblacion.append(lista_line[2])
+            porcen_vacunacion.append(lista_line[3])
+            porcen_vacunacion_comple.append(lista_line[4])
+            num_vacunas_día.append(lista_line[5])
+
 
 def max_min():
     n=int(input('¿Cuántos países quiere ver? (El número mencionado será igual para ambas categorías): '))
