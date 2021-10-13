b5753633759b4208485c3c0eba2522b43d317a51
IvanaEspinoza <A01742392@itesm.mx>
2021-10-13 05:19:54 +0000

    proy

--- assignments/00HelloWorld/src/exercise.py
+++ assignments/00HelloWorld/src/exercise.py
@@ -1,9 +1,10 @@
 def formar_matriz():
 
-def max():
-
-def min():
+def max_min():
+    n=input('¿Cuántos países quiere ver (El número mencionado será igual para ambas categorías)?')
     
+def graficar():
+
 def main():
     #escribe tu código abajo de esta línea
     ejecutar = input('''Seleccione una opción: 
@@ -12,8 +13,13 @@ def main():
     3. Gráfico de comparación entre población completamente vacunada y el PIB del país''')
 
     if ejecutar == '1':
-        n=input('¿Cuántos países quiere ver (El número mencionado será igual para ambas categorías)?')
-        
+        max_min()
+    elif ejecutar == '2':
+        max_min()
+    elif ejecutar == '3':
+        graficar()
+    else:
+        print('Entrada inválida')
 
 if __name__=='__main__':
     main()
